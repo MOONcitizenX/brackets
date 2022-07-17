82f750aac4535204d7e8b298c918e7ce2e1b2791
MOONcitizenX <mooncitizenx@gmail.com>
2022-07-17 11:19:07 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+	const arr = str.split('');
+	for (let _ = 0; _ < str.length; _++) {
+		for (let i = 0; i < bracketsConfig.length; i++) {
+			for (let j = 0; j < arr.length; j++) {
+				if (
+					arr[j] === bracketsConfig[i][0] &&
+					arr[j + 1] === bracketsConfig[i][1]
+				) {
+					arr.splice(j, 2);
+				}
+			}
+		}
+	}
+	return arr.length === 0;
+};
